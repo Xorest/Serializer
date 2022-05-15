3844dfbd0d9a2c109a1bac653483e827a8c4729b
Artem <at.10@inbox.ru>
2022-05-15 11:04:31 +0300

    Encapsulation class Data

--- Serializer/Data.cpp
+++ Serializer/Data.cpp
@@ -1,1 +1,29 @@
 #include "Data.h"
+
+Data::Data(uint64_t a, bool b, uint64_t c)
+	: _a(a),
+	  _b(b),
+	  _c(c)
+{
+}
+
+uint64_t Data::A() const
+{
+	return _a;
+}
+
+bool Data::B() const
+{
+	return _b;
+}
+
+uint64_t Data::C() const
+{
+	return _c;
+}
+
+template<class Serializer>
+int Data::serializer(Serializer& serializer)
+{
+	return serializer(_a, _b, _c);
+}
--- Serializer/Data.h
+++ Serializer/Data.h
@@ -1,11 +1,19 @@
 #pragma once
 //it is very big class!!!
 #include <iostream>
+
 class Data
 {
 public:
-    uint64_t a;
-    bool b;
-    uint64_t c;
+    Data(uint64_t a, bool b, uint64_t c);
+    uint64_t A() const;
+    bool B() const;
+    uint64_t C() const;
+    template <class Serializer>
+    int serializer(Serializer& serializer);
+private:
+    uint64_t _a;
+    bool _b;
+    uint64_t _c;
 };
 
